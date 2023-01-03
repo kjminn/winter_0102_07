d25a69c832e31a489ea097a27dadf93459354861
kjminn <kjminn2001@gmail.com>
2023-01-03 15:48:05 +0900

    completion

---
+++ src/teread/slideshow/ImageThread.java
@@ -0,0 +1,36 @@
+package teread.slideshow;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class ImageThread extends Thread {
+	JLabel lbl;
+	int time = 1000;
+	
+	public ImageThread(JLabel lbl) {
+		this.lbl = lbl;
+	}
+		
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	@Override
+	public void run() {
+		int i = 0;
+		ImageIcon icon;
+		while(true) {
+			icon = new ImageIcon("images/idol0"+ ++i + ".jpg");
+			lbl.setIcon(icon);
+			
+			if(i == 5) {
+				i = 0;
+			}
+			try {
+				sleep(time);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
